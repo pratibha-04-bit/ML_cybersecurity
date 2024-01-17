54b54d5c64927f0f9c18f1d0f7d7a552751eeb42
pratibha-04-bit <79438911+pratibha-04-bit@users.noreply.github.com>
2024-01-18 00:00:02 +0530

    Update vt_report.py
    api key

--- vt_report.py
+++ vt_report.py
@@ -4,7 +4,7 @@ url = "https://www.virustotal.com/api/v3/urls/aHR0cDovLzA1eXpiLXVwcXMudXM"
 
 headers = {
     "accept": "application/json",
-    "x-apikey": "982ce415cf8ef4cae0c948f2eb9dddd2cb68119f7a57d5e95c5af8b66f7dcde7"
+    "x-apikey": ""
 }
 
 response = requests.get(url, headers=headers)
